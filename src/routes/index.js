98fdd35428ba80fcc0764a2040213344f36c73ce
discoverilla <info@discoverilla.com>
2022-10-04 20:14:15 -0400

    Initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+/**
+ * Index router file
+ *
+ * @package   backend/src/routes
+ * @author    Taras Hryts <streaming9663@gmail.com>
+ * @author    WangTuring <wangwang@turing.com>
+ * @copyright 2020 Say Digital Company
+ * @license   Turing License
+ * @version   2.0
+ * @link      https://turing.ly
+ */
+
+const express = require('express')
+const router = express.Router()
+
+const galaxyController = require('../controller/galaxyController');
+
+router.get('/galaxy/getOne', galaxyController.getOne)
+
+/**
+ * Authentication page API router
+ */
+
+module.exports = router
