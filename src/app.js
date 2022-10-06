6e895538599bde68b5a8fbb6ffb70e38a549d9bf
discoverilla <info@discoverilla.com>
2022-10-06 06:59:55 -0400

    Add upload module

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@
 var express = require('express')
 var path = require('path')
 var cors = require('cors')
-var multer  = require('multer')
 
 var bodyParser = require('body-parser')
 const dotenv = require('dotenv')
@@ -20,26 +19,11 @@ var apiRouter = require('./routes/index')
 dotenv.config()
 const app = express()
 
-var storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-     cb(null, '../uploads')
-   },
-   filename: function (req, file, cb) {
-     cb(null, file.originalname)
-   }
-})
-
-var upload = multer({ storage: storage })
-
-
 app.use(express.json())
 app.use('/public', express.static(path.join(__dirname, '../public/upload')))
-app.use('/uploads', express.static('uploads'))
 
 app.use(express.static(path.join(__dirname, 'assets')))
-// app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-// app.use(cors({origin: 'https://discoverilla.com'}))
 const corsOpts = {
   origin: '*',
 
@@ -64,8 +48,4 @@ app.use(function (err, req, res, next) {
     return res.status(500).json(err)
 })
 
-module.exports = {
-   app: app, // or whatever you want to assign it to
-   upload: upload // again, set it to what you like
-};
-// module.exports = app
+module.exports = app
